d033e344faa2c538b9e04a42cb3d3299d0011737
zhuopeijin <zhuopeijin@bytedance.com>
2022-09-04 20:40:44 +0800

    init navigation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,17 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
-  base: "/static_page/"
+  base: "/static_page/",
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  themeConfig: {
+    logo: '/logo.png',
+     nav: [
+      { text: 'Home', link: '/' },
+      { text: '杂谈', link: '/杂谈/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+  
 }
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,18 @@
-# Hello VuePress
+---
+home: true
+#heroImage: /logo.png
+heroText: 一个博客
+tagline: 卓的博客
+actionText: 目录 →
+actionLink: /
+features:
+- title: iOS
+  details: detail1
+- title: Web
+  details: detail2
+- title: Others
+  details: details
+footer: MIT Licensed
+---
 
-![image](./images/icon.png)
+# Hello VuePress
