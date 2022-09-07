52a31fcd8f8183c9ddcd25b5926a7abc4106e540
zhuopeijin <zhuopeijin@bytedance.com>
2022-09-07 12:18:02 +0800

    add 新教伦理与资本主义精神 add DIP

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,30 +1,37 @@
 module.exports = {
-  title: 'Blog',
-  description: '记录一些东西',
-  base: "/static_page/",
-  head: [
-    ['link', { rel: 'icon', href: '/logo.png' }]
-  ],
-  themeConfig: {
-    logo: '/logo.png',
-     nav: [
-      { text: 'Home', link: '/' },
-      { text: '杂谈', link: '/杂谈/' },
-      { text: '哲学', items:[
-              { text : '宋明理学15讲', link: '/哲学/宋明理学15讲'}
-          ]
-      },
-      { text: 'External', link: 'https://google.com' },
-    ],
-    sidebar: 'auto'
-    /*{
-      '/' : [
-          '/',
-          '/哲学/'
-      ],
-      '/哲学/' : [
-          '宋明理学15讲'
-      ]
-    }*/
-  }
+	title: 'Blog',
+	description: '记录一些东西',
+	base: "/static_page/",
+	head: [
+		['link', { rel: 'icon', href: '/logo.png' }]
+	],
+	themeConfig: {
+		logo: '/logo.png',
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: '杂谈', link: '/杂谈/' },
+			{
+				text: 'Tech', items: [
+					{ text: 'DIP', link: '/Tech/DIP' }
+				]
+			},
+			{
+				text: '哲学', items: [
+					{ text: '宋明理学15讲', link: '/哲学/宋明理学15讲' },
+					{ text: '新教伦理与资本主义精神', link: '/哲学/新教伦理与资本主义精神' }
+				]
+			},
+			{ text: 'External', link: 'https://google.com' },
+		],
+		sidebar: 'auto'
+		/*{
+		  '/' : [
+			  '/',
+			  '/哲学/'
+		  ],
+		  '/哲学/' : [
+			  '宋明理学15讲'
+		  ]
+		}*/
+	}
 }
